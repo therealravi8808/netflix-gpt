4f958d9b09000af342e93ecfcaf9b096c43de33a
Ravi Kumar Gupt <87171097+therealravi8808@users.noreply.github.com>
2023-09-26 15:05:58 +0530

    netflix

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import Login from './Login'
 import Browse from "./Browse"
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom' 
+
+
+
 const Body = () => {
+
+const appRouter=createBrowserRouter([
+    {
+        path:"/",
+        element:<Login/>
+    },
+    {
+        path:"/Browse",
+        element:<Browse/>
+    },
+
+])
+
   return (
     <div>
-<Login/>
-<Browse/>
+        <RouterProvider router={appRouter} />
+
     </div>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Header from './Header'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    
+    <div>
+        <Header/>
+        <div>
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/f85718e8-fc6d-4954-bca0-f5eaf78e0842/ea44b42b-ba19-4f35-ad27-45090e34a897/IN-en-20230918-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+alt="logo"/>
+</div>
+        Login
+        </div>
   )
 }
 
