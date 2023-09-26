0f970023bd5f31bb5730c9346c50f461369506fb
Ravi Kumar Gupt <87171097+therealravi8808@users.noreply.github.com>
2023-09-26 19:31:49 +0530

    netflix

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,12 +6,13 @@ const Login = () => {
     
     <div>
         <Header/>
-        <div>
+        <div className='absolute'>
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/f85718e8-fc6d-4954-bca0-f5eaf78e0842/ea44b42b-ba19-4f35-ad27-45090e34a897/IN-en-20230918-popsignuptwoweeks-perspective_alpha_website_large.jpg"
 alt="logo"/>
 </div> 
 
-<form>
+<form className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0'>
+<h1>Sign In</h1>
     <input type="text" placeholder='Email Address' className='p-2 m-2'/>
     <input type='password' placeholder='Password' className='p-2 m-2'/>
 <button className='p-4 m-4 '>Sign In</button>
