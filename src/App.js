e7cb81f82fb316801ccb0a59906da6941a643a81
therealravi8808 <itsforravi@gmail.com>
2023-10-03 17:10:16 +0530

    netflix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 
+import { Provider } from 'react-redux';
 import Body from './components/Body';
+import appStore from './utilis/appStore';
 
 function App() {
   return (
-  <Body/>
-    
+  <Provider store={appStore} >
+    <Body/>
+    </Provider>
   );
 }
 
