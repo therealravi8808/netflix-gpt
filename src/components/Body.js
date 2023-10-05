23a5fbef7f7b1527af2ed657213fa2ea61ff3026
therealravi8808 <itsforravi@gmail.com>
2023-10-05 18:48:21 +0530

    netflix

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './Login'
 import Browse from "./Browse"
 import { createBrowserRouter } from 'react-router-dom'
 import { RouterProvider } from 'react-router-dom' 
-
+import {  onAuthStateChanged } from "firebase/auth";
+import { auth } from '../utilis/firebase'
+import { useDispatch } from 'react-redux'
 
 
 const Body = () => {
+const dispatch=useDispatch()
 
 const appRouter=createBrowserRouter([
     {
@@ -18,7 +21,22 @@ const appRouter=createBrowserRouter([
         element:<Browse/>
     },
 
-])
+]);
+
+useEffect(()=>{
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const {uid,email,displayName} = user;
+      
+
+
+    } else {
+      
+
+
+    }
+  });
+},[]);
 
   return (
     <div>
