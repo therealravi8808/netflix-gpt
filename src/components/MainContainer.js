7fc2033638b161c4ffd55db7626bf6b7f1da413d
therealravi8808 <itsforravi@gmail.com>
2023-10-16 12:57:25 +0530

    netflix

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,12 +5,15 @@ import VideoBackground from "./VideoBackground";
 const MainContainer=()=>{
   const movies=useSelector(store=>store.movies?.nowPlayingMovies);
 
-  if(movies==null) return;
+  if(!movies) return;
+
 const mainMovie=movies[0];
 console.log(mainMovie);
 
+const {original_title,overview}=mainMovie;
+
     return <div>
-       <VideoTitle/>
+       <VideoTitle title={original_title} overview={overview}/>
        <VideoBackground/>
 
 
