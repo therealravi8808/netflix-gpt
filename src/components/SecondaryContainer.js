1c7bb0d3813181e03bea65137de223baa852c85b
therealravi8808 <itsforravi@gmail.com>
2023-10-19 12:00:24 +0530

    netflix

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -8,12 +8,14 @@ const movies=useSelector(store=>store.movies);
 
   return (
     movies.nowPlayingMovies &&
-   ( <div className='-mt-52 relative z-20 bg-black '>
+   ( <div className=' bg-black '>
+    <div className='-mt-52 relative z-20'>
 <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
 <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
 <MovieList title={"Popular"} movies={movies.nowPlayingMovies} />
 <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies} />
 <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
+</div>
 </div>)
   )
 }
