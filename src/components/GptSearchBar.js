904e6dda68fcc9308d9db1ed3d0957b6769bda25
therealravi8808 <itsforravi@gmail.com>
2024-05-05 13:32:11 +0530

    gptslice update

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import lang from '../utilis/languageConstants';
+import React from "react";
+import lang from "../utilis/languageConstants";
+import { useSelector } from "react-redux";
 
 const GptSearchBar = () => {
+    
+  const langKey=useSelector(store=>store.config.lang)
+
   return (
-    <div className='pt-[8%] flex justify-center' >
-      <form className='w-1/2  bg-black grid grid-cols-12' >
-  <input type='text' className='p-4 m-4 col-span-9 ' 
-  placeholder={lang.hindi.gptSearchPlaceholder}/>
-  <button className='col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg'>
-   {lang.hindi.search}
-    </button>
+    <div className="pt-[8%] flex justify-center">
+      <form className="w-1/2  bg-black grid grid-cols-12">
+        <input
+          type="text"
+          className="p-4 m-4 col-span-9 "
+          placeholder={lang[langKey].gptSearchPlaceholder}
+        />
+        <button className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg">
+          {lang[langKey].search}
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default GptSearchBar;
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,7 @@ const Header = () => {
 
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
   const handleSignOut = () => {
     signOut(auth)
@@ -58,19 +59,21 @@ const Header = () => {
       <img className="w-44" src={LOGO} alt="logo" />
       {user && (
         <div className="flex p-2">
-          <select
-            className="p-2 m-2 bg-gray-900 text-white rounded-2xl"
-            onChange={handleLanguageChange}
-          >
-            {SUPPORTED_LANGUAGES.map((lang) => (
-              <option key={lang.identifier} value={lang.identifier}>
-                {lang.name}
-              </option>
-            ))}
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white rounded-2xl"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
 
-            {/* <option value="hindi">Hindi</option> 
+              {/* <option value="hindi">Hindi</option> 
    <option value="spanish">Spanish</option>  */}
-          </select>
+            </select>
+          )}
           <button
             className="py-2 px-4 mx-4 my-2 bg-purple-800
  text-white rounded-lg "
